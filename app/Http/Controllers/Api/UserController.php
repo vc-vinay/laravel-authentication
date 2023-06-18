8be3dad33960acafc2127bde07d1199e12ce0419
vc-vinay <vinay.prajapati@viitor.cloud>
2023-06-18 16:30:26 +0530

    Apply pipeline filter

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Constant\Constant;
 use App\Http\Controllers\Controller;
+use App\QueryFilters\Active;
 use App\Http\Resources\UserCollection;
 use App\Models\User;
+use App\QueryFilters\Sort;
 use Illuminate\Http\Request;
+use Illuminate\Pipeline\Pipeline;
 
 class UserController extends Controller
 {
@@ -28,8 +31,16 @@ class UserController extends Controller
         return Constant::STATUS_TRUE;
     }
 
-    public function getUsers() {
-        $users = User::paginate(1); // Fetch users data
+    public function getUsers()
+    {
+        $users = app(Pipeline::class)
+            ->send(User::query())
+            ->through([
+                Active::class,
+                Sort::class,
+            ])
+            ->thenReturn()
+            ->paginate(5);
 
         return (new UserCollection($users))->additional([
             'status' => true,
